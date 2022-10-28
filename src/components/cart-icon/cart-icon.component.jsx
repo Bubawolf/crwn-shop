001690d3d7487f16de08ce1b8490522a5b6e2a25
Bubawolf <ibotero@bamboostudio.com.co>
2022-10-28 17:11:59 -0500

    Add products to cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,11 +5,18 @@ import { ToogleCartContext } from "../../context/toogle-cart.context";
 
 const CartIcon = () => {
   const { toggleCart, setToggleCart } = useContext(ToogleCartContext);
+  const { cartItems } = useContext(ToogleCartContext);
+
+  const finalQuantity = cartItems
+    .map((item) => item.quantity)
+    .reduce((acc, cur) => acc + cur, 0);
+  //console.log(finalQuantity, "here is the quantity");
+
   const toogleIsCartOpen = () => setToggleCart(!toggleCart);
   return (
     <div className="cart-icon-container" onClick={toogleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{finalQuantity}</span>
     </div>
   );
 };
--- src/context/toogle-cart.context.jsx
+++ src/context/toogle-cart.context.jsx
@@ -1,16 +1,43 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  // Find if cartItems contains productoAdd
+  //console.log(cartItems, productToAdd, "here is the error");
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+  if (existingCartItem) {
+    // If found, increment quantity
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  //return new array with modified cartItems/ new cart Item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 // As the actual value that you want to access
 export const ToogleCartContext = createContext({
   toggleCart: false,
   setToggleCart: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 // we allaways need a provider
 export const ToogleCartProvider = ({ children }) => {
   const [toggleCart, setToggleCart] = useState(false);
-  const value = { toggleCart, setToggleCart };
-  console.log(value);
+
+  //console.log(value);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    //console.log(productToAdd, cartItems);
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = { toggleCart, setToggleCart, addItemToCart, cartItems };
 
   return (
     <ToogleCartContext.Provider value={value}>
